e3565780d0c81045676187b8a74fb77487fc4f4c
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2014-01-27 14:19:39 +0000

    Refactor the fixtures out.

--- test/htwax/test_link.rb
+++ test/htwax/test_link.rb
@@ -2,52 +2,50 @@ require 'test_helper'
 
 module HtWax
   describe Link do
-    describe '[]' do
-      let(:link) { Link.new(q: 'htwax') }
+    let(:link) { Link.new }
+    let(:q)    { Link.new(q: 'htwax') }
 
+    describe '[]' do
       it 'returns the value' do
-        link[:q].must_equal 'htwax'
-        link['q'].must_equal 'htwax'
+        q[:q].must_equal 'htwax'
+        q['q'].must_equal 'htwax'
       end
 
       it 'returns nil if the key has no value' do
-        link[:nonexistent].must_be_nil
-        link['nonexistent'].must_be_nil
-        link[nil].must_be :nil?
+        q[:nonexistent].must_be_nil
+        q['nonexistent'].must_be_nil
+        q[nil].must_be :nil?
       end
     end
 
     describe '[]=' do
-      subject { Link.new }
-
       it 'sets new values' do
-        subject[:arg] = 'some value'
-        subject['arg2'] = 'another value'
+        link[:arg] = 'some value'
+        link['arg2'] = 'another value'
 
-        subject[:arg].must_equal 'some value'
-        subject['arg'].must_equal 'some value'
-        subject[:arg2].must_equal 'another value'
-        subject['arg2'].must_equal 'another value'
+        link[:arg].must_equal 'some value'
+        link['arg'].must_equal 'some value'
+        link[:arg2].must_equal 'another value'
+        link['arg2'].must_equal 'another value'
       end
     end
 
     describe 'reset' do
-      subject { Link.new(q: 'htwax') }
 
       it 'allows preset values to be set and cleared' do
-        subject[:q] = 'REST'
-        subject[:q].must_equal 'REST'
-        subject.reset
-        subject[:q].must_equal 'htwax'
+        q[:q] = 'REST'
+        q[:q].must_equal 'REST'
+        q.reset
+        q[:q].must_equal 'htwax'
       end
 
       it 'allows non-preset values to be set and cleared' do
-        subject[:arg] = 'REST'
-        subject[:q].must_equal 'htwax'
-        subject[:arg].must_equal 'REST'
-        subject.reset
-        subject[:q].must_equal 'htwax'
-        subject[:arg].must_be_nil
+        q[:arg] = 'REST'
+        q[:q].must_equal 'htwax'
+        q[:arg].must_equal 'REST'
+        q.reset
+        q[:q].must_equal 'htwax'
+        q[:arg].must_be_nil
       end
     end
   end
